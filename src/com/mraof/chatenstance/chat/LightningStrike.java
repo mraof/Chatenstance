63a2973bf915eda2609ee9389d64d084e68431db
mraof <mraoflink@verizon.net>
2014-05-15 15:53:59 -0400

    Thu May 15 15:53:59 EDT 2014

--- src/com/mraof/chatenstance/chat/LightningStrike.java
+++ src/com/mraof/chatenstance/chat/LightningStrike.java
@@ -1,11 +1,13 @@
 package com.mraof.chatenstance.chat;
 
-import net.minecraft.client.renderer.entity.RenderManager;
+import java.util.Random;
+
 import net.minecraft.entity.effect.EntityLightningBolt;
 import net.minecraftforge.event.ServerChatEvent;
 
 public class LightningStrike extends ChatHandler
- {
+{
+	Random rand = new Random();
 
 	@Override
 	public void handleMessage(ServerChatEvent event) 
@@ -14,16 +16,18 @@ public class LightningStrike extends ChatHandler
 		int matches = 0;
 		for(String word : words)
 		{
-			if(word.equalsIgnoreCase("lightning") || word.equalsIgnoreCase("lightening"))
+			if(word.equalsIgnoreCase("lightning") || word.equalsIgnoreCase("lightening") || word.equalsIgnoreCase("strike"))
 				matches++;
 		}
-		if(matches > 1)
+		if(.05 * matches > rand.nextDouble())
 		{
-			EntityLightningBolt bolt = new EntityLightningBolt(event.player.worldObj, event.player.posX, event.player.worldObj.getPrecipitationHeight((int) event.player.posX, (int) event.player.posZ), event.player.posZ);
+			double x = event.player.posX + rand.nextInt(17) - 8;
+			double z = event.player.posZ + rand.nextInt(17) - 8;
+			EntityLightningBolt bolt = new EntityLightningBolt(event.player.worldObj, x, event.player.worldObj.getPrecipitationHeight((int) x, (int) z), z);
 			event.player.worldObj.addWeatherEffect(bolt);
-			RenderManager.instance.renderEntitySimple(bolt, 1);
+
 		}
-			
+
 	}
 
 }
