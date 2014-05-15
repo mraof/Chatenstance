56213e4d5338486e913588379478161474023189
mraof <mraoflink@verizon.net>
2014-05-15 12:57:09 -0400

    Thu May 15 12:57:09 EDT 2014

--- src/com/mraof/chatenstance/chat/ChatParser.java
+++ src/com/mraof/chatenstance/chat/ChatParser.java
@@ -1,13 +1,22 @@
 package com.mraof.chatenstance.chat;
 
+import java.util.ArrayList;
+
 import net.minecraftforge.event.ServerChatEvent;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
 public class ChatParser
 {
+	ArrayList<ChatHandler> handlers = new ArrayList<ChatHandler>();
 	@SubscribeEvent
 	public void onChatMessage(ServerChatEvent event)
 	{
+		for(ChatHandler handler : handlers)
+			handler.handleMessage(event);
+	}
+	public void addHandler(ChatHandler handler)
+	{
+		handlers.add(handler);
 	}
 }
