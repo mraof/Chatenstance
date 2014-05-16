441e50dc1e0632c692120c3f46460b952b46d17f
mraof <mraoflink@verizon.net>
2014-05-15 22:22:19 -0400

    Thu May 15 22:22:19 EDT 2014

--- src/com/mraof/chatenstance/chat/PlantTalk.java
+++ src/com/mraof/chatenstance/chat/PlantTalk.java
@@ -16,12 +16,12 @@ public class PlantTalk extends ChatHandler
 		int lowercase = 0;
 		for(int i = 0; i < event.message.length(); i++)
 			lowercase += Character.isUpperCase(event.message.charAt(i)) ? -1 : 1;
+		int y = (int) event.player.posY;
 		for(int x = (int) event.player.posX - 4; x < event.player.posX + 4; x++)
-		for(int y = (int) event.player.posY - 4; y < event.player.posY + 4; y++)
 		for(int z = (int) event.player.posZ - 4; z < event.player.posZ + 4; z++)
 		{
 			Block block;
-			if((block = event.player.worldObj.getBlock(x, y, z)) instanceof IGrowable && lowercase > rand.nextInt(event.message.length()))
+			if((block = event.player.worldObj.getBlock(x, y, z)) instanceof IGrowable && lowercase > rand.nextInt(event.message.length() * 16))
 				((IGrowable) block).func_149853_b(event.player.worldObj, rand, x, y, z);
 		}
 
