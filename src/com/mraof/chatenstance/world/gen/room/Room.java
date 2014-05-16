d59e051efc2b40423d37eecfdced00c910124dc1
mraof <mraoflink@verizon.net>
2014-05-16 18:07:35 -0400

    Fri May 16 18:07:35 EDT 2014

---
+++ src/com/mraof/chatenstance/world/gen/room/Room.java
@@ -0,0 +1,16 @@
+package com.mraof.chatenstance.world.gen.room;
+
+import net.minecraft.world.chunk.IChunkProvider;
+
+public class Room
+{
+	public Room()
+	{
+	}
+
+	public void generate(IChunkProvider provider, int chunkX, int chunkY)
+	{
+		provider.canSave()
+	}
+}
+
