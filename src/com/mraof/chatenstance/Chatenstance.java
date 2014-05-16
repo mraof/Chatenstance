a634aec651b44139a0d4de3063dcf717a3af01e7
mraof <mraoflink@verizon.net>
2014-05-16 09:37:04 -0400

    Fri May 16 09:37:04 EDT 2014

--- src/com/mraof/chatenstance/Chatenstance.java
+++ src/com/mraof/chatenstance/Chatenstance.java
@@ -1,6 +1,7 @@
 package com.mraof.chatenstance;
 
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.config.Configuration;
 
 import com.mraof.chatenstance.chat.ChatParser;
 import com.mraof.chatenstance.chat.LightningStrike;
@@ -11,15 +12,23 @@ import com.mraof.chatenstance.chat.PlantTalk;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = "Chatenstance", name = "Chatenstance", version = "@VERSION@")
 public class Chatenstance
 {
 	ChatParser chatParser;
+	@EventHandler 
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		chatParser = new ChatParser();
+		Configuration config = new Configuration(event.getSuggestedConfigurationFile());
+		config.load();
+		if(config.get("Parts", "LightningStrike", true).getB
+	}
 	@EventHandler
 	public void load(FMLInitializationEvent event)
 	{
-		chatParser = new ChatParser();
 		MinecraftForge.EVENT_BUS.register(chatParser);
 		chatParser.addHandler(new LightningStrike());
 		chatParser.addHandler(new MobHate());
