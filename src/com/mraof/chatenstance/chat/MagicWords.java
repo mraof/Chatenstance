c06e66c3967c00f973e8e7cb5d248c7e39764275
mraof <mraoflink@verizon.net>
2014-05-15 22:52:23 -0400

    Thu May 15 22:52:23 EDT 2014

--- src/com/mraof/chatenstance/chat/MagicWords.java
+++ src/com/mraof/chatenstance/chat/MagicWords.java
@@ -1,13 +1,22 @@
 package com.mraof.chatenstance.chat;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
+import net.minecraft.potion.PotionEffect;
 import net.minecraftforge.event.ServerChatEvent;
 
 public class MagicWords extends ChatHandler
- {
+{
+	public static HashMap<String, PotionEffect> effectWords = new HashMap<String, PotionEffect>();
 
 	@Override
-	public void handleMessage(ServerChatEvent event) {
-		// TODO Auto-generated method stub
-
+	public void handleMessage(ServerChatEvent event) 
+	{
+		String[] words = event.message.split(" ");
+		HashSet<String> matches = new HashSet<String>();
+		for(String word : words)
+			if(effectWords.containsKey(word))
+				matches.add(word);
 	}
 }
