805ff88488fb81fe8a72de1dbdc79b0360a5a061
mraof <mraoflink@verizon.net>
2014-05-16 15:07:23 -0400

    Fri May 16 15:07:23 EDT 2014

--- src/com/mraof/chatenstance/chat/DimensionWords.java
+++ src/com/mraof/chatenstance/chat/DimensionWords.java
@@ -1,7 +1,11 @@
 package com.mraof.chatenstance.chat;
 
+import java.util.Iterator;
+
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.network.play.server.S07PacketRespawn;
+import net.minecraft.network.play.server.S1DPacketEntityEffect;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.world.WorldServer;
 
 public class DimensionWords extends ChatHandler
@@ -32,6 +36,31 @@ public class DimensionWords extends ChatHandler
 		WorldServer currentWorldServer = player.mcServer.worldServerForDimension(currentDimension);
 		player.dimension = dimension;
 		WorldServer destinationWorldServer = player.mcServer.worldServerForDimension(dimension);
-		S07PacketRespawn respawnPacket = new S07PacketRespawn(player.dimension, player.worldObj.difficultySetting, destionationW
+		S07PacketRespawn respawnPacket = new S07PacketRespawn(player.dimension, player.worldObj.difficultySetting, destinationWorldServer.getWorldInfo().getTerrainType(), player.theItemInWorldManager.getGameType());
+		player.playerNetServerHandler.sendPacket(respawnPacket);
+
+		currentWorldServer.removePlayerEntityDangerously(player);
+		player.isDead = false;
+
+		destinationWorldServer.spawnEntityInWorld(player);
+		double destinationY = destinationWorldServer.getPrecipitationHeight((int) player.posX, (int) player.posZ);
+		player.setLocationAndAngles(player.posX, destinationY, player.posZ, player.rotationYaw, player.rotationPitch);
+		destinationWorldServer.updateEntityWithOptionalForce(player, false);
+		player.setWorld(destinationWorldServer);
+
+		currentWorldServer.getPlayerManager().removePlayer(player);
+		destinationWorldServer.getPlayerManager().addPlayer(player);
+		destinationWorldServer.theChunkProviderServer.loadChunk((int) player.posX >> 4, (int) player.posZ >> 4);
+		player.playerNetServerHandler.setPlayerLocation(player.posX, destinationY, player.posZ, player.rotationYaw, player.rotationPitch);
+		player.theItemInWorldManager.setWorld(destinationWorldServer);
+		player.mcServer.getConfigurationManager().updateTimeAndWeatherForPlayer(player, destinationWorldServer);
+		player.mcServer.getConfigurationManager().syncPlayerInventory(player);
+		Iterator<?> iterator = player.getActivePotionEffects().iterator();
+
+		while(iterator.hasNext())
+		{
+			PotionEffect potionEffect = (PotionEffect) iterator.next();
+			player.playerNetServerHandler.sendPacket(new S1DPacketEntityEffect(player.getEntityId(), potionEffect));
+		}
 	}
 }
