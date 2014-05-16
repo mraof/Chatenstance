8dbeb5fbf6374e714a732b8c9eaac6bf9c7860fb
mraof <mraoflink@verizon.net>
2014-05-16 11:22:10 -0400

    Fri May 16 11:22:10 EDT 2014

---
+++ src/com/mraof/chatenstance/block/ChatBox.java
@@ -0,0 +1,51 @@
+package com.mraof.chatenstance.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+public class ChatBox extends Block
+{
+	public ChatBox()
+	{
+		super(Material.rock);
+		setHardness(1.0F);
+	}
+
+	@Override
+	public boolean renderAsNormalBlock()
+	{
+		return false;
+	}
+	
+	@Override
+	public int getRenderType()
+	{
+		return -1;
+	}
+
+	@Override
+	public boolean isOpaqueCube()
+	{
+		return false;
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void registerBlockIcons(IIconRegister iconRegister)
+	{
+		iconRegister.registerIcon("chatenstance:chatbox");
+	}
+
+	@Override
+	public TileEntity createTileEntity(World world, int metadata) 
+	{
+		return new TileEntityChatBox();
+	}
+}
+
