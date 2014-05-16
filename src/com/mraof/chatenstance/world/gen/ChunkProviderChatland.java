3f4eae86b125370ec8830d6844140350d19315fb
mraof <mraoflink@verizon.net>
2014-05-16 16:52:31 -0400

    Fri May 16 16:52:31 EDT 2014

--- src/com/mraof/chatenstance/world/gen/ChunkProviderChatland.java
+++ src/com/mraof/chatenstance/world/gen/ChunkProviderChatland.java
@@ -38,7 +38,7 @@ public class ChunkProviderChatland implements IChunkProvider
 			{
 				for(y = 0; y < 2; y++)
 					chunkBlocks[x * 4096 | z * 256 | y] = Blocks.bedrock;
-				for(;y < 20 + (16 - Math.abs(8 - x ) - Math.abs(8 - z)) / 2; y++)
+				for(;y < 64 + (16 - Math.abs(8 - x ) - Math.abs(8 - z)) / 2 + (chunkX * chunkZ) % 27; y++)
 					chunkBlocks[x * 4096 | z * 256 | y] = Blocks.sandstone;
 			}
 		Chunk chunk = new Chunk(this.world, chunkBlocks, chunkMetadata, chunkX, chunkZ);
