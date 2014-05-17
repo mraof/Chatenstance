5ca2458487fac59b50939d01fb3571e8e676a75f
mraof <mraoflink@verizon.net>
2014-05-16 22:40:34 -0400

    Fri May 16 22:40:34 EDT 2014

--- src/com/mraof/chatenstance/world/gen/room/RoomEmptyBig.java
+++ src/com/mraof/chatenstance/world/gen/room/RoomEmptyBig.java
@@ -15,19 +15,17 @@ public class RoomEmptyBig extends Room
 	public void generate(ChunkProviderChatland chatland, int chunkX, int chunkZ)
 	{
 		super.generate();
-		double[] ids = new double[9];
-		chatland.noiseGen0.generateNoiseOctaves(ids, chunkX - 1, chunkZ - 1, 3, 3, 1.0D, 1.0D, 1.0D);
-		int x = ((int) ids[2]) == -1 ? 0 : 1;
-		int z = ((int) ids[4]) == -1 ? 0 : 1;
-		int zMax = ((int) ids[6]) == -1 ? 15 : 16;
-		int xMax = ((int) ids[8]) == -1 ? 15 : 16;
+		double[][] ids = new double[9][1];
+		for(int i = 0; i < 9; i++)
+			chatland.noiseGen0.generateNoiseOctaves(ids[i], chunkX + i / 3, chunkZ + i % 3, 1, 1, 1.0D, 1.0D, 1.0D);
+		int x = ((int) ids[2][0]) == -1 ? 0 : 1;
+		int zMin = ((int) ids[4][0]) == -1 ? 0 : 1;
+		int zMax = ((int) ids[6][0]) == -1 ? 15 : 16;
+		int xMax = ((int) ids[8][0]) == -1 ? 15 : 16;
 
-		for(; x < xMax + 15; x++)
-			for(; z < zMax; z++)
+		for(; x < xMax; x++)
+			for(int z = zMin; z < zMax; z++)
 				for(int y = 20; y < 34; y++)
-				{
 					setBlock(x, y, z, Blocks.air);
-					System.out.println(x + " " + y + " " + z + " | " + xMax + " " + zMax);
-				}
 	}
 }
