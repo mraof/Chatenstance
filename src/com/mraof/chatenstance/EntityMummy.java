d6387119a6e5b6ac7cf14351367dd56d6a025d2e
mraof <mraoflink@verizon.net>
2014-05-17 13:52:39 -0400

    Sat May 17 13:52:39 EDT 2014

---
+++ src/com/mraof/chatenstance/EntityMummy.java
@@ -0,0 +1,13 @@
+package com.mraof.chatenstance.entity;
+
+import net.minecraft.entity.monster.EntityZombie;
+import net.minecraft.world.World;
+
+public class EntityMummy extends EntityZombie
+ {
+
+	public EntityMummy(World par1World) {
+		super(par1World);
+		// TODO Auto-generated constructor stub
+	}
+}
