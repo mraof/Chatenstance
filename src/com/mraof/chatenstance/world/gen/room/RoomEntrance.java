50ec6ba20ccc84408a07229ab6b4eb611470fb65
mraof <mraoflink@verizon.net>
2014-05-16 23:56:18 -0400

    Fri May 16 23:56:18 EDT 2014

--- src/com/mraof/chatenstance/world/gen/room/RoomEntrance.java
+++ src/com/mraof/chatenstance/world/gen/room/RoomEntrance.java
@@ -20,5 +20,11 @@ public class RoomEntrance extends Room {
 			for(int z = 1; z < 15; z++)
 				for(int y = 20; y < 34; y++)
 					setBlock(x, y, z, Blocks.air);
+		int y = 20;
+		for(; chunkBlocks[4096 * 8 | 256 * 8 | y] != null; y++)
+		{
+			setBlock(8, y, 8, Blocks.ladder);
+			setBlock(9, y, 8, Blocks.sandstone);
+		}
 	}
 }
