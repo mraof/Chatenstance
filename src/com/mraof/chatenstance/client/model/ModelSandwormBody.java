f2254ed0c0a9c0e1310f0a77da8edcd30c569066
mraof <mraoflink@verizon.net>
2014-05-18 15:51:34 -0400

    Sun May 18 15:51:34 EDT 2014

--- src/com/mraof/chatenstance/client/model/ModelSandwormBody.java
+++ src/com/mraof/chatenstance/client/model/ModelSandwormBody.java
@@ -21,6 +21,14 @@ public class ModelSandwormBody extends ModelBase
 	@Override
 	public void render(Entity entity, float par2, float par3, float par4, float par5, float par6, float par7)
 	{
+		this.setRotationAngles(par2, par3, par4, par5, par6, par7, entity);
 		this.segment.render(par7);
+	}
+	
+	@Override
+	public void setRotationAngles(float par1, float par2, float par3, float par4, float par5, float par6, Entity entity)
+	{
+		this.segment.rotateAngleX = -entity.rotationPitch;
+		this.segment.rotateAngleY = -entity.rotationYaw;
 	}
 }
