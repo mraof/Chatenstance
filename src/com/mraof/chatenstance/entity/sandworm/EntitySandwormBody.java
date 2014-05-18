54e910fb007e2b0742b6f3304fb583ea66ec4241
mraof <mraoflink@verizon.net>
2014-05-18 03:44:54 -0400

    Sun May 18 03:44:54 EDT 2014

--- src/com/mraof/chatenstance/entity/sandworm/EntitySandwormBody.java
+++ src/com/mraof/chatenstance/entity/sandworm/EntitySandwormBody.java
@@ -15,4 +15,19 @@ public class EntitySandwormBody extends EntitySandwormPart
 		this.head = entitySandwormHead;
 		this.place = place;
 	}
+	public void updatePartPosition() 
+	{
+		if(this.place > 0)
+		{
+			EntitySandwormPart part = head.parts.get(place - 1);
+			double diffX = this.posX - part.posX;
+			double diffY = this.posY - part.posY;
+			double diffZ = this.posZ - part.posZ;
+			double ratio = this.width / Math.sqrt(diffX * diffX + diffY * diffY + diffZ * diffZ);
+			this.posX = part.posX + diffX * ratio;
+			this.posY = part.posY + diffY * ratio;
+			this.posZ = part.posZ + diffZ * ratio;
+		}
+
+	}
 }	
--- src/com/mraof/chatenstance/entity/sandworm/EntitySandwormPart.java
+++ src/com/mraof/chatenstance/entity/sandworm/EntitySandwormPart.java
@@ -35,23 +35,20 @@ public abstract class EntitySandwormPart extends EntityCreature
 
 	public void updatePartPosition() 
 	{
-		if(this.place > 0)
-		{
-			EntitySandwormPart part = head.parts.get(place - 1);
-			double diffX = this.posX - part.posX;
-			double diffY = this.posY - part.posY;
-			double diffZ = this.posZ - part.posZ;
-			double ratio = this.width / Math.sqrt(diffX * diffX + diffY * diffY + diffZ * diffZ);
-			this.posX = part.posX + diffX * ratio;
-			this.posY = part.posY + diffY * ratio;
-			this.posZ = part.posZ + diffZ * ratio;
-		}
-
 	}
 	@Override
 	public void setDead()
 	{
 		super.setDead();
-		System.out.println(place + " died");
+		System.out.println(place + " died (" + worldObj.isRemote + ")");
+		if(worldObj.isRemote)
+		{
+			try
+			{
+				Integer crash = null;
+				crash.byteValue();
+			} catch(Exception e)
+			{ e.printStackTrace(); }
+		}
 	}
 }
