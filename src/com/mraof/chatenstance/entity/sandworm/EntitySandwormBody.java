bb18b1d8c7ef8582ba24e349a8390f399e6ab245
mraof <mraoflink@verizon.net>
2014-05-18 13:06:01 -0400

    Sun May 18 13:06:01 EDT 2014

---
+++ src/com/mraof/chatenstance/entity/sandworm/EntitySandwormBody.java
@@ -0,0 +1,73 @@
+package com.mraof.chatenstance.entity.sandworm;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.world.World;
+
+import com.mraof.chatenstance.entity.sandwormold.EntitySandwormHead;
+
+public class EntitySandwormBody extends EntityLiving
+{
+	public EntitySandwormHead head;
+	int place;
+	
+	public EntitySandwormBody(World world)
+	{
+		super(world);
+	}
+
+	public EntitySandwormBody(EntitySandwormHead head, int place)
+	{
+		super(head.worldObj);
+		this.head = head;
+		this.place = place;
+	}
+
+	@Override
+	public void writeEntityToNBT(NBTTagCompound tagCompound)
+	{
+		if(this.head != null)
+			super.writeEntityToNBT(tagCompound);
+		else
+			this.setDead();
+	}
+
+	@Override
+	public void onUpdate() 
+	{
+		super.onUpdate();
+		if(this.head == null || (this.head).isDead)
+		{
+			this.setDead();
+		}
+		else
+		{
+			EntityLiving part = head.parts.get(place - 1);
+			double diffX = this.posX - part.posX;
+			double diffY = this.posY - part.posY;
+			double diffZ = this.posZ - part.posZ;
+			double ratio = this.width / Math.sqrt(diffX * diffX + diffY * diffY + diffZ * diffZ);
+			this.setPosition(part.posX + diffX * ratio, part.posY + diffY * ratio, part.posZ + diffZ * ratio);
+		}
+	}
+	//@Override
+	//public void setDead()
+	//{
+	//	super.setDead();
+	//	if(this.head != null && !this.head.isDead)
+	//		head.setDead();
+	//}
+	
+	@Override
+	public boolean isEntityEqual(Entity entity)
+	{
+		return this == entity || this.head == entity;
+	}
+
+	@Override
+	protected boolean canDespawn()
+	{
+		return false;
+	}
+}
